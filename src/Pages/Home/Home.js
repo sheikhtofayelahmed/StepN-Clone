f2e7d04d08b0cd89c5c4dcf2752f9276b6004a88
coding-sta <personal.sta.bd@gmail.com>
2022-06-15 09:51:21 +0600

    second part done

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Home.css"
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='icon'>
+                <img src="https://stepn.com/img/logo.svg" alt="" />
+            </div>
+            <div className="join breath">DOWNLOAD NOW</div>
+            <div className="menu">
+                <span className="menu-item"></span>
+                <span className="menu-item"></span>
+                <span className="menu-item"></span>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Second/Second.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Second.css";
+const Second = () => {
+  return (
+    <div className="second">
+      <div className="title">
+        <div>JOIN TODAY + MAKE</div>
+        <div>YOUR STEPS COUNT!</div>
+      </div>
+      <p>
+        <span>STEPN IS A WEB 3 LIFESTYLE APP</span>
+        <span>WITH SOCIAL-FI AND GAME-FI ELEMENTS</span>
+      </p>
+      <p>
+        <span>PLAYERS CAN MAKE HANDSOME EARNINGS BY</span>
+        <span>WALKING, JOGGING, OR RUNNING OUTDOORS</span>
+      </p>
+    </div>
+  );
+};
+
+export default Second;
