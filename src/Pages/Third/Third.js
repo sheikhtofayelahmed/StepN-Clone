e78ebfe333fcff4949a09eaae88879e17570fc24
coding-sta <personal.sta.bd@gmail.com>
2022-06-15 10:52:50 +0600

    third done

--- src/Pages/Third/Third.js
+++ src/Pages/Third/Third.js
@@ -1,28 +1,42 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./Third.css";
 const Third = () => {
+  const [distance, setDistance] = useState(49123245234.66);
+  useEffect(() => {
+    setDistance(distance + 0.00583);
+  }, [distance]);
+  const [carbon, setCarbon] = useState(3289318.66);
+  useEffect(() => {
+    setCarbon(carbon + 0.000000383);
+  }, [carbon]);
+  const [calories, setCalories] = useState(3053502126);
+  useEffect(() => {
+    setCalories(calories + 0.000253);
+  }, [calories]);
   return (
     <div className="third">
       <div class="pc">
         <img src="https://stepn.com/img/shoeLines.svg" alt="" />
         <div class="data">
           <div class="item">
-            <span>49,123,245,234.66</span>
+            <span>{distance.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
             <span>DISTANCE RUN (M)</span>
           </div>
           <div class="item">
-            <span>3,289,317.0625</span>
+            <span>{carbon.toFixed(4).toString().replace(/\B(?=(\d{4})+(?!\d))/g, ",")}</span>
             <span>CARBON OFFSET (KG)</span>
           </div>
           <div class="item">
-            <span>3,053,500,923.786</span>
+            <span>{calories.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
             <span>CALORIES BURNED (KCAL)</span>
           </div>
           <div class="item">
             <span>806,530</span>
             <span>
               DISCORD MEMBERS
-              <a href="https://discord.com/invite/stepn2" >
+              <a href="https://discord.com/invite/stepn2">
                 <img src="https://stepn.com/img/discorddata.svg" alt="" />{" "}
               </a>
             </span>
